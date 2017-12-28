c672db0db12ddc699ad5e7d1f2aa5950905ca70c
Javier Jimenez <javierrcc522@gmail.com>
2017-12-27 22:18:05 -0800

    add yargs and encoded the addresss call for the doctor

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs.options({
+  a: { demand: true, alias: 'addressArgv', describe: 'Address to fetch weather for', string: true}
+})
+.help().alias('help', 'h').argv;
+
+var encodedAddress = encodeURIComponent(argv.addressArgv)
 
 request({
-  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia',
+  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
   json: true
 }, (error, response, body) => {
   // console.log(JSON.stringify(body, undefined, 2));
