ab0d50f5d36f7e00a199349974f90eaa3d6c6253
Javier Jimenez <javierrcc522@gmail.com>
2017-12-29 01:51:36 -0800

    add error profing

--- app.js
+++ app.js
@@ -12,8 +12,14 @@ request({
   url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
   json: true
 }, (error, response, body) => {
-  // console.log(JSON.stringify(body, undefined, 2));
-  console.log(`Address; ${body.results[0].formatted_address}`);
-  console.log(`Address; ${body.results[0].geometry.location.lat}`);
-  console.log(`Address; ${body.results[0].geometry.location.lng}`);
+  if (error){
+    console.log('Unable to connect to googles API');
+  } else if (body.status === 'ZERO_RESULTS') {
+    console.log('The address provided does not exit');
+  } else if (body.status === 'OK') {
+    // console.log(JSON.stringify(body, undefined, 2));
+    console.log(`Address; ${body.results[0].formatted_address}`);
+    console.log(`Address; ${body.results[0].geometry.location.lat}`);
+    console.log(`Address; ${body.results[0].geometry.location.lng}`);
+  };
 });
