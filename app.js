16df2cf9e9ce3f200a5853bb7faff75e525580c6
Javier Jimenez <javierrcc522@gmail.com>
2017-12-29 19:44:33 -0800

    chain call backs geocode and weather together

--- app.js
+++ app.js
@@ -16,18 +16,17 @@ const argv = yargs
   .alias('help', 'h')
   .argv;
 
-// geocode.geocodeAddress(argv.address, (errorMessage, results) => {
-//   if (errorMessage) {
-//     console.log(errorMessage)
-//   } else {
-//     console.log(JSON.stringify(results, undefined, 2 ));
-//   };
-// });
-
-weather.getWeather(37.8267, -122.4233, (errorMessage, weatherResults) => {
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
   if (errorMessage) {
-    console.log(errorMessage);
+    console.log(errorMessage)
   } else {
-    console.log(JSON.stringify(weatherResults, undefined, 2));
-  }
+    console.log(results.address);
+    weather.getWeather(results.latitude, results.longitude, (errorMessage, weatherResults) => {
+      if (errorMessage) {
+        console.log(errorMessage);
+      } else {
+        console.log(`It's currently ${weatherResults.temperature}. It feels like ${weatherResults.apparentTemperature}.`);
+      }
+    });
+  };
 });
