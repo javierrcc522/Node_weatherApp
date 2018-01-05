fc99ae4f2226ecfcaa432a544959d0c0e5c68b8f
Javier Jimenez <javierrcc522@gmail.com>
2018-01-04 21:28:02 -0800

    add commit lol

--- playground/promise.js
+++ playground/promise.js
@@ -1,13 +1,34 @@
-var somePromise = new Promise(function(resolve, reject){
-  setTimeout(()=>{
-    resolve("It's working ;p");
-  }, 2500);
+var asyncAdd = (a, b) => {
+  return new Promise(function(resolve, reject){
+    setTimeout(function(){
+      if(typeof a === 'number' && typeof b === 'number'){
+        resolve(a + b);
+      } else {
+        reject('Arguments must be numbers');
+      }
+    }, 2500);
+  });
+};
+
+asyncAdd(9, 19).then(function(works){
+  console.log('Result', works);
+}, function(noWork){
+  console.log(noWork);
 });
 
 
-somePromise.then(
-  (resolve)=>{
-  console.log('Success:', resolve);
-},function(reject){
-  console.log('Not bueno', reject);
-});
+
+
+// var somePromise = new Promise(function(resolve, reject){
+//   setTimeout(()=>{
+//     resolve("It's working ;p");
+//   }, 2500);
+// });
+//
+//
+// somePromise.then(
+//   (resolve)=>{
+//   console.log('Success:', resolve);
+// },function(reject){
+//   console.log('Not bueno', reject);
+// });
