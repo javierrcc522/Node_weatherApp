1e05584961d41370c71d9793411ec9059a4bd21a
Javier Jimenez <javierrcc522@gmail.com>
2018-01-07 14:27:13 -0800

    chain async promises to the same level

--- app-promise.js
+++ app-promise.js
@@ -20,7 +20,17 @@ axios.get(geocodeURL).then((response)=>{
   if (response.data.status === 'ZERO_RESULTS') {
     throw new Error('Unable to find the address.');
   }
-  console.log(response.data);
+
+  var lat = response.data.results[0].geometry.location.lat;
+  var lng = response.data.results[0].geometry.location.lng;
+  var weatherUrl = `https://api.darksky.net/forecast/b9fc6bbcf61ab4b286ff86a222b0428a/${lat},${lng}`;
+  console.log(response.data.results[0].formatted_address);
+  return axios.get(weatherUrl);
+
+}).then((response)=>{
+  var temperature = response.data.currently.temperature;
+  var apparentTemperature = response.data.currently.apparentTemperature;
+  console.log(`It's currently ${temperature}. It feels like ${apparentTemperature}.`);
 }).catch((error)=>{
   if (error.code === 'ENOTFOUND'){
     console.log('Unable to connect to the API server.');
